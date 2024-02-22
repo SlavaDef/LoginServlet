ace5d7fcb8b985f5d147980ccf11e6322564e8cd
SlavaDef <slavadef@gmail.com>
2024-02-22 22:25:50 +0200

    new com 5

--- src/main/java/home/Casino.java
+++ src/main/java/home/Casino.java
@@ -13,29 +13,29 @@ import java.util.List;
 
 @WebServlet("/casino")
 public class Casino extends HttpServlet {
-
+    List<String> answers = new ArrayList<>();
+    List<String> answers2 = new ArrayList<>();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
+
         String login = req.getParameter("login");
         String play = req.getParameter("play");
         String play2 = req.getParameter("play2");
 
-        List<String> answers = new ArrayList<>();
+
         answers.add(play);
-        List<String> answers2 = new ArrayList<>();
+
         answers2.add(play2);
 
-        try {
-            writer.println( "<p>" + login +" you chose: " + answers + "for " +answers.size()+ " times"+ "</p>");
-            writer.println("<p>" + login + " you chose: " + answers2 + "for " +answers2.size()+ " times"+ "</p>");
-            // writer.println("<h3>Join in home <a href="+"index2.jsp"+"> -Page- </a></h3>");
-            writer.println("<h3>Join in home <a href= \" index2.jsp  \"> -Page- </a></h3>");
+        try (PrintWriter writer = resp.getWriter()) {
 
-        } finally {
-            writer.close();
+            writer.println("<p>" + login + " you chose: " + answers + "for " + answers.size() + " times" + "</p>");
+            writer.println("<p>" + login + " you chose: " + answers2 + "for " + answers2.size() + " times" + "</p>");
+            // writer.println("<h3>Join in home <a href="+"index2.jsp"+"> -Page- </a></h3>");
+            writer.println("<h3>Join in questions <a href= \" index2.jsp  \"> -Page- </a></h3>");
+
         }
     }
 }
